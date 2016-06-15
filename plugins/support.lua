d8b11778f2228b72468ba3b798957539aea810c4
amirreza <negahapp@chmail.ir>
2016-06-16 01:02:58 +0430

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 119626024
+  local user = 226238411
 
   if matches[1] == "support" then
     user = 'user#id'..user
