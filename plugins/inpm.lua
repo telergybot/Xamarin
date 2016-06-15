fa08347cbaba1e1e0c5e9a6e189e79a6164348ff
amirreza <negahapp@chmail.ir>
2016-06-16 00:49:35 +0430

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-	text = "❌ فقط در سوپر گروه ها فعال است.\n\nبرای خرید گروه ضد اسپم با زامارین\nبه آیدی @AmirDark پیام بدهیـد."
+	text = "❌فقط در سوپر گروه ها فعال است /n برای خرید گروه با اپسون با ایدی زیر در ارتباط باشید : /n @mr_flat "
      	return text
     end
 
